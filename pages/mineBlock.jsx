e5c428debe674b62d9486b9c02f75f30470ed9c3
UXNation <uxnation.inc@gmail.com>
2022-03-26 15:06:21 +0530

    final

--- pages/mineBlock.jsx
+++ pages/mineBlock.jsx
@@ -64,7 +64,7 @@ export default function mineBlock(){
                  }
                  else{
                    if(selectedNode!==undefined){
-                     let x = Math.floor(Math.random()*501)+1
+                     let x = Math.floor(Math.random()*501)+101;
                      mine(nodes[selectedNode], x);
                    }
                    else{
